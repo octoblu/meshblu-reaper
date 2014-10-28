0455c2d4614d1e0246d042185991347e38597a98
kmariano <koshin.mariano@gmail.com>
2014-10-28 14:38:07 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+var MESSAGE_SCHEMA = {
+  type: 'object',
+  properties: {
+    exampleBoolean: {
+      type: 'boolean',
+      required: true
+    },
+    exampleString: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+var OPTIONS_SCHEMA = {
+  type: 'object',
+  properties: {
+    firstExampleOption: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+function Plugin(){
+  this.options = {};
+  this.messageSchema = MESSAGE_SCHEMA;
+  this.optionsSchema = OPTIONS_SCHEMA;
+  return this;
+}
+util.inherits(Plugin, EventEmitter);
+
+Plugin.prototype.onMessage = function(message){
+  var payload = message.payload;
+  this.emit('message', {devices: ['*'], topic: 'echo', payload: payload});
+};
+
+Plugin.prototype.setOptions = function(options){
+  this.options = options;
+};
+
+module.exports = {
+  messageSchema: MESSAGE_SCHEMA,
+  optionsSchema: OPTIONS_SCHEMA,
+  Plugin: Plugin
+};
