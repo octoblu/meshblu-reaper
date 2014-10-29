162cbe34c8b535a7c545d01320546e8b5e27618c
redaphid <iam@aaronherres.com>
2014-10-29 11:14:10 -0700

    added skynet driver for reaper

---
+++ reaper-skynet.js
@@ -0,0 +1,30 @@
+var skynet = require('skynet');
+var exec = require('child_process').exec;
+var config = require('./config.json');
+
+var Reaper = require('./reaper');
+
+var r = new Reaper();
+
+
+var conn = skynet.createConnection({
+  uuid: config.uuid,
+  token: config.token
+});
+
+conn.on('notReady', function(data){
+  console.log('UUID FAILED AUTHENTICATION!');
+  console.log(data);
+});
+
+conn.on('ready', function(data){
+  console.log('UUID AUTHENTICATED!');
+  console.log(data);
+});
+
+r.connect(function(){
+  conn.on('message', function(message){
+    console.log(message.payload);
+    r[message.payload.action](message.payload.data);
+  });
+});
