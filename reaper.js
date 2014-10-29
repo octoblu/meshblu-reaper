546998968db548c19880da241b59f05251b40c89
redaphid <iam@aaronherres.com>
2014-10-29 13:13:50 -0700

    made the reaper's pins a little more configurable

--- reaper.js
+++ reaper.js
@@ -1,27 +1,27 @@
 var _     = require('lodash');
 var raspi = require('raspi-io');
 var exec  = require('child_process').exec;
-
+var eyesPin = 16, jawPin = 18;
 var Reaper = function(){
   var self = this;
 
   self.connect = function(callback){
     self.board = new raspi();
     self.board.on('ready', function(){
-      self.board.pinMode(7,  self.board.MODES.OUTPUT);
-      self.board.pinMode(29, self.board.MODES.OUTPUT);
-      callback();
+      self.board.pinMode(eyesPin,  self.board.MODES.OUTPUT);
+      self.board.pinMode(jawPin, self.board.MODES.OUTPUT);
+      if(callback) callback();
     });
   };
 
   self.eyes = function(on){
     var value = on ? self.board.HIGH : self.board.LOW;
-    self.board.pins[29].value = value;
+    self.board.pins[eyesPin].value = value;
   };
 
   self.jaws = function(on){
     var value = on ? self.board.HIGH : self.board.LOW;
-    self.board.pins[7].value = value;
+    self.board.pins[jawPin].value = value;
   };
 
   self.say = function(text){
