1dccf5fdfa4e25233e44fe862a3106a27cc6dddd
redaphid <iam@aaronherres.com>
2014-10-29 13:20:37 -0700

    initial commit of the voice sh

---
+++ voice.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+espeak -k220 -s 120 -p 10 -v english-us "how was your Day? i mean, your Last Day! hahahahahahaahaha" &
+espeak -k999  -s 120 -p 50 -v english-us "how was your day? i mean, your last day! Hahahahahahaahaha"&
+espeak  -s 120 -p 100 -v english-us "how was your day? i mean, your last day! Hahahahahahaahaha"
